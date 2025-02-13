cf3935b4ac49a3077713ef2923055d5a1ad355fa
Rene <rener2104@gmail.com>
2025-02-12 19:17:56 -0600

    Add sorting algorithm

--- tam_sorter/algorithm.py
+++ tam_sorter/algorithm.py
@@ -12,3 +12,21 @@ def SWAP_SERVER(arr, i, j ):
 
 
 def SORT_TAM_SERVER(arr):
+    '''
+    Sorts array using Dutch National Flag Algorithm so that all 'T' elements appear first, followed by all 'A' elements, then by  all 'M' elements.
+    :param arr: The list of character to be sorted.
+    :return: The sorted array.
+    '''
+    low, mid, high = 0, 0, len(arr) - 1
+    while mid <= high:
+        if arr[mid] == 'T':
+            SWAP_SERVER(arr, low, mid)
+            low += 1
+            mid += 1
+        elif arr[mid] == 'A':
+            mid += 1
+        elif arr[mid] == 'M':
+            SWAP_SERVER(arr, high, mid)
+            high -= 1
+    return arr
+
