13905fed277e08ec260de9d137fc2353d70dd66c
Rene <rener2104@gmail.com>
2025-02-16 23:18:04 -0600

    Add check for invalid inputs

--- tam_sorter/networking.py
+++ tam_sorter/networking.py
@@ -2,7 +2,6 @@
 import socket
 from .algorithm import SORT_TAM_SERVER
 
-
 def start_server():
     HOST = '127.0.0.1'
     PORT = 9187
@@ -15,24 +14,34 @@ def start_server():
         while True:
             conn, addr = s.accept()
             with conn:
-                data = conn.recv(1024).decode().strip()
-                if not data.endswith('#'):
-                    response = "Input does not end with #"
-                else:
+                while True:
+                    data = conn.recv(1024).decode().strip()
+                    if not data:
+                        break  # Exit loop if connection is closed
                     datalist = list(data.strip('#'))
                     sorted_list = SORT_TAM_SERVER(datalist)
                     response = ''.join(sorted_list)
-                conn.send(response.encode())
-
+                    conn.send(response.encode())
 
 def start_client():
     HOST = '127.0.0.1'
     PORT = 9187
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        sequence = input("Enter sequence of 'T', 'A', 'M' letters ending with '#': ")
 
-        s.send(sequence.encode())
+        while True:
+            print("Enter sequence of 'T', 'A', 'M' letters ending with '#':")
+            sequence = input()
 
-        response = s.recv(1024).decode()
-        print("Response from server: ", response)
+            if set(sequence) - {'T', 'A', 'M', '#'}:
+                print("Invalid input! Only 'T', 'A', 'M', and '#' are allowed.")
+                continue
+            if not sequence.endswith('#'):
+                print("Input does not end with #")
+                continue
+
+            s.send(sequence.encode())
+
+            response = s.recv(1024).decode()
+            print("Response from server:")
+            print(response)
