5e2ac030ef4393e2f27e8521f177f169b3120a5d
Rene <rener2104@gmail.com>
2025-02-16 23:18:56 -0600

    Add argument for running GUI

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import pytest
 from tam_sorter.networking import start_client, start_server
+from gui import gui_main
 
 if __name__ == '__main__':
+
     if len(sys.argv) < 2:
-        print("Usage: python main.py [client|server]")
+        print("Usage: python main.py [start|client|server|tests]")
         sys.exit(1)
     if sys.argv[1] == 'client':
         start_client()
@@ -12,7 +14,8 @@ if __name__ == '__main__':
         start_server()
     elif sys.argv[1] == 'tests':
         sys.exit(pytest.main(["tests.py", "-v"]))
+    elif sys.argv[1] == 'start':
+        gui_main.show_overview()
     else:
-        print("Usage: python main.py [client|server|tests]")
+        print("Usage: python main.py [start|client|server|tests]")
         sys.exit(1)
-
